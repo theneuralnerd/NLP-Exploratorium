7cc5da61d2bdb80c695aaa3d772a17ef6c5af7d9
kushagraSrivastava <kushagrasrivastava.fac@aesl.in>
2023-05-28 20:13:28 +0530

    Adding some RNNs of temp data

---
+++ config/config.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+BASE_DIR = Path("/Users/kushagra/Documents/personal-repo/NLP/NLP-Exploratorium")
+DATA_DIR = Path(BASE_DIR, 'data')
+MODEL_WEIGHTS = Path(BASE_DIR, "model_weights")
